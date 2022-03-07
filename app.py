0bdb942f5a4ebf829282aac67b80b8f09e77ee6c
JazRad <jazrad@inthevale.net>
2022-03-07 16:38:19 +0800

    initial hello-world app

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+from flask import flask
+APP = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    return 'Hello, World from Flask on ECS!'
+
+if __name__ = '__main__':
+    app.run(host='0.0.0.0', port=8080)
