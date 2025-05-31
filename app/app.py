7b3f2da83661a733032176ce58a1632936b3d177
Jarrad <jazrad@inthevale.net>
2025-05-31 22:55:08 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,9 +1,15 @@
 from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World from Flask on ECS!'
+def create_app():
+    app = Flask(__name__)
+    
+    @app.route('/')
+    def hello_world():
+        return 'Hello, World from Flask on ECS!'
+    
+    return app
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
